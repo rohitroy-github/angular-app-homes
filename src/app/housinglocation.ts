177df4cb71027e99b120e237e4c5c67375391079
rohitroy@github <rhtry.tech@gmail.com>
2025-02-20 16:35:17 +0530

    add: first commit [running]

---
+++ src/app/housinglocation.ts
@@ -0,0 +1,12 @@
+// creating new interface
+// https://angular.dev/tutorials/first-app/04-interfaces
+export interface HousingLocation {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+  }
