e62f58cefe4bcbf2610268bec6a9fd03880efb7e
rohitroy@github <rhtry.tech@gmail.com>
2025-02-20 19:16:32 +0530

    update: routing to details page [running]

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -4,21 +4,33 @@ import { CommonModule } from "@angular/common";
 
 import { HousingLocation } from "../housinglocation";
 
+import { RouterLink, RouterModule } from "@angular/router";
+
 @Component({
   selector: "app-housing-location",
-  imports: [CommonModule],
-  template: ` <section class="listing">
-  <img
-    class="listing-photo"
-    [src]="housingLocation.photo"
-    alt="Exterior photo of {{ housingLocation.name }}"
-    crossorigin
-  />
-  <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-  <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
-</section>
-`,
-styleUrls: ['./housing-location.component.css'],
+  imports: [CommonModule, RouterModule, RouterLink],
+  template: `
+    <section class="listing">
+      <a [routerLink]="['/detials', housingLocation.id]">
+        <img
+          class="listing-photo"
+          [src]="housingLocation.photo"
+          alt="Exterior photo of {{ housingLocation.name }}"
+          crossorigin
+        />
+        <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation.city }}, {{ housingLocation.state }}
+        </p>
+      </a>
+
+      <button [routerLink]="['/details', housingLocation.id]" class="learn-more-btn">
+        Learn More
+      </button>
+      
+    </section>
+  `,
+  styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
